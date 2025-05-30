4bcc6c81b2ef2da0700decd119951a1dd3284d78
Genesis Vegas <vagrant@VMPruebas>
2025-05-30 19:17:50 +0000

    ADD: Punto D

---
+++ Punto_D.sh
@@ -0,0 +1,10 @@
+
+mkdir -p ~/Ejercicio_D/{rx,tx}
+for i in {1..20}; do
+  mkdir ~/Ejercicio_D/rx/lotes_$i
+  mkdir ~/Ejercicio_D/tx/lotes_$i
+done
+for i in {1..100}; do
+  mkdir ~/Ejercicio_D/rx/extras_$i
+done
+
