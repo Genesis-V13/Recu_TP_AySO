b6ec19fd1a939d86cfc9b0b3d22ec8d5129ebdc8
Génesis <genesisvega.az@gmail.com>
2026-05-27 03:18:39 -0300

    Update Punto_A.sh

--- Punto_A.sh
+++ Punto_A.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 # Punto A - Estructuras simétricas
-# Creamos la estructura en la raíz usando expansión de llaves
 
 mkdir -p /datos/{textos/{libros,revistas},videos/{peliculas,series}}
-
