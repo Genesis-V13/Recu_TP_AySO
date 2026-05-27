53ac73308f6b8fb0bf35c3fb370d81a0d6880ae4
Génesis <genesisvega.az@gmail.com>
2026-05-27 03:54:16 -0300

    Refactor usuario.sh comments and clean up code
    Updated comments and removed unnecessary lines.

--- usuario.sh
+++ usuario.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-# Trabajo Practico - Creacion de Usuario (Punto B)
+# Creacion de Usuario (Punto B)
 
-# Creamos el grupo secundario primero
 groupadd p1c1_2026_g1
 
-# Creamos el usuario asignando el grupo y la clave usando openssl
 useradd -m -g p1c1_2026_g1 -p $(echo "clave1" | openssl passwd -1 -stdin) p1c1_2026_u1
