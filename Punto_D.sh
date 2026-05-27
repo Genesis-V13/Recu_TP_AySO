5aeae7aa9a94cfce5a555f99ab48211099456993
Génesis <genesisvega.az@gmail.com>
2026-05-27 03:22:56 -0300

    Update Punto_D.sh

--- Punto_D.sh
+++ Punto_D.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 # Trabajo Practico - Punto D: Estructuras Asimetricas
-# Creacion de la jerarquia solicitada en el home usando expansion de llaves en un solo comando
 
 mkdir -p ~/Ejercicio_D/{rx/{lotes_{1..20},extras_{1..100}},tx/lotes_{1..20}}
