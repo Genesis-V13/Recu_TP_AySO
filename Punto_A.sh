d6b9a56bff4730cca50947d34028ec73df8a546d
Genesis-V13 <genesisvega.az@gmail.com>
2026-05-27 02:10:15 -0300

    ADD: Archivo del Punto A listo

---
+++ Punto_A.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Punto A - Estructuras simétricas
+# Creamos la estructura en la raíz usando expansión de llaves
+
+mkdir -p /datos/{textos/{libros,revistas},videos/{peliculas,series}}
+
